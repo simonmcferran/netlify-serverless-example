02a8c42cf935082812a85600d8083b7f7acabe93
Simon McFerran <simon@white.space>
2021-03-23 12:16:51 +0000

    testing ENV

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,8 @@
 // functions/hello.js
 exports.handler = async event => {
-  const subject = event.queryStringParameters.name || 'World'
+  const { MY_NAME } = process.env
+
+  const subject = event.queryStringParameters.name || MY_NAME
   return {
     statusCode: 200,
     body: `Hello ${subject}!`,
